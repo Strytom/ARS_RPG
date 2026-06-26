4d12a9620f6c264d4ccb24c978e7cbe218f47b08
Mamikon <Marushchakmamikon@gmail.com>
2026-06-26 18:34:48 +0300

    Initial Commit

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private InputSystem_Actions _control;
+
+    [Header("Player atributes")]
+    public float _speed;
+
+    private void Awake() {
+        _rb = GetComponent<Rigidbody>();
+        _control = new InputSystem_Actions();
+    }
+    private void FixedUpdate() {
+        
+    }
+}
